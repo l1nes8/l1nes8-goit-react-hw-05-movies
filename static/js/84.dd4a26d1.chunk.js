527e4eec48e626a4b8c516a5850ed3fe4a16d91e
l1nes8 <126972112+l1nes8@users.noreply.github.com>
2023-10-12 19:24:55 +0000

    Deploying to gh-pages from @ l1nes8/l1nes8-goit-react-hw-05-movies@bb1821bc020b340e1ec5e13041bc27138c1d3351 🚀

---
+++ static/js/84.dd4a26d1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[84],{84:function(e,t,r){r.r(t);r(791);var c=r(184);t.default=function(){return(0,c.jsx)("div",{children:(0,c.jsx)("h2",{children:"\u0414\u0435\u0442\u0430\u043b\u044c\u043d\u0430 \u0456\u043d\u0444\u043e\u0440\u043c\u0430\u0446\u0456\u044f \u043f\u0440\u043e \u0444\u0456\u043b\u044c\u043c"})})}}}]);
+//# sourceMappingURL=84.dd4a26d1.chunk.js.map
