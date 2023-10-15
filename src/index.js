c7ae917e0b4c6235ae5deb26bd44b73acf402e2f
l1nes8 <egor1glavatyje@gmail.com>
2023-10-15 22:54:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="l1nes8-goit-react-hw-05-movies">
       <App />
     </BrowserRouter>
   </React.StrictMode>
