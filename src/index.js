6767faff41af8ed856deeb3dda5cf5b2ae7a35de
l1nes8 <egor1glavatyje@gmail.com>
2023-10-15 21:37:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
       <App />
-    </React.StrictMode>
-  </BrowserRouter>
+    </BrowserRouter>
+  </React.StrictMode>
 );
